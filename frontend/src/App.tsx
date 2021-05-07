dce0ff96003523ca95eebfb4c41e39ea4ad7ee49
Tatiana <hello@tatianacodes.com>
2021-05-06 22:10:14 -0700

    initializes app basic styling and browser router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Router from './components/Router';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Router />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
