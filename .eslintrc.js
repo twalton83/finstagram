530c51f101875247d9c794b225c3cc8aaa50c7ce
Tatiana <hello@tatianacodes.com>
2021-05-06 22:08:39 -0700

    sets up project configurations

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript',
+    'prettier',
+  ],
+};
